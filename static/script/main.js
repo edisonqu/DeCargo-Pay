d4305cd6dea45556828ebbbeba95ce2487678608
edison <edisonquu@gmail.com>
2022-10-22 20:41:23 -0400

    just form details for now... was able to get the information from html to js

---
+++ static/script/main.js
@@ -0,0 +1,14 @@
+
+function takeValue() {
+
+    var vendor = document.getElementById("vendor").value
+    var type = document.getElementById("type").value
+    var number = document.getElementById("number").value
+    var totalAmount = document.getElementById("totalAmount").value
+
+    console.log(vendor)
+    document.write(vendor)
+    document.write(type)
+    document.write(number)
+    document.write(totalAmount)
+}
